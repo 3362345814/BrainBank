c68915215b6dc573af6523e38669bf441c4a1d4a
cityseason <3362345814@qq.com>
2025-04-19 15:35:19 +0800

    Initial commit

---
+++ src/main/java/com/cityseason/brainbank/BrainBankApplication.java
@@ -0,0 +1,13 @@
+package com.cityseason.brainbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cityseason/brainbank/BrainBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cityseason.brainbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
